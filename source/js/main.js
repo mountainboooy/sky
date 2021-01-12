fb776daa5e017c3b19fd2b2417812af1177e4bf1
mountainboooy <yoshiharatakeru@gmail.com>
2021-01-13 00:00:50 +0900

    menu behavior for header

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,10 @@
+window.onload = () => {
+  console.log('load')
+  const headerToggle = document.getElementById('header__toggle')
+  headerToggle.onclick = toggleMenu
+}
 
-function test () {
+function toggleMenu () {
+  const header = document.getElementById('header')
+  header.classList.toggle('menu-open')
 }
