0bd8866c117c95f456a184a95ed01445ec012fc8
mountainboooy <yoshiharatakeru@gmail.com>
2021-01-22 21:48:40 +0900

    rank

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,21 @@
 window.onload = () => {
-  console.log('load')
   const headerToggle = document.getElementById('header__toggle')
   headerToggle.onclick = toggleMenu
+  addRank()
 }
 
 function toggleMenu () {
   const header = document.getElementById('header')
   header.classList.toggle('menu-open')
+}
+
+function addRank () {
+  const box = document.getElementById('popular-posts-box')
+  const items = Array.from(box.getElementsByClassName('popular-posts-item'))
+  items.forEach((item, i) => {
+    const rankElement = document.createElement('div')
+    rankElement.classList = 'popular-posts__rank'
+    rankElement.innerHTML = i
+    item.append(rankElement)
+  })
 }
