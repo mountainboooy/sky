fd046171ab218103cc9e1a93619c2dc60df40721
mountainboooy <yoshiharatakeru@gmail.com>
2021-08-01 08:00:55 +0900

    amazon asosieit

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,6 @@
-function sum (a, b) {
-  return a + b
-}
+hexo.extend.tag.register('amazon', function(args){
+    const id = 'mountainboo0a-22'
+    const asin = args[0];
+    const title = args[1];
+    return `<p><a href="https://www.amazon.co.jp/dp/${asin}/?tag=${id}"><img src="https://m.media-amazon.com/images/P/${asin}.jpg" alt="${title}" width="400"><br>${title}</a></p>`
+});
